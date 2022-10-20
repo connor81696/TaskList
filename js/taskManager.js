99abb6d531d55a52c9e44fb61dee4d9b03e562fc
connor81696 <connor81696@gmail.com>
2022-10-20 19:21:58 -0400

    Update taskManager.js

--- js/taskManager.js
+++ js/taskManager.js
@@ -3,8 +3,38 @@ class TaskManager {
     constructor(tasks) {
       this.tasks = tasks;
     }
+
     TaskManager() {
       console.log(this.tasks);
     }
+
+  constructor(currentId = 0) {
+    this.currentId = currentId
+    this.status = 'TODO'
+    this.tasks = [];
   }
+<<<<<<< Updated upstream
  
+=======
+
+  addTask(obj) {
+    this.currentId++
+    const id = this.currentId
+    const status = this.status
+    this.tasks = [...this.tasks, { id, ...obj, status }]
+    console.log(this.tasks);
+  }
+
+  }
+
+const x = new TaskManager;
+x.addTask({
+  name: "Do dishes",
+  assignedTo: "Connor",
+  priority: "Med Priority",
+  description: "description 1",
+  dueDate: "08-17-2022"
+})
+
+
+>>>>>>> Stashed changes
